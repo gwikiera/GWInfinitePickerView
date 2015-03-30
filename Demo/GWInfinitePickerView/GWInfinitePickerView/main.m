d2b1b7d22721a173c098d77b3f11033732e3f216
Grzegorz Wikiera <gwi@trifork.com>
2015-03-30 14:05:07 +0200

    Initial implementation of GWInfinitePickerView

---
+++ Demo/GWInfinitePickerView/GWInfinitePickerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GWInfinitePickerView
+//
+//  Created by Grzegorz Wikiera on 30.03.2015.
+//  Copyright (c) 2015 Grzegorz Wikiera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Demo/GWInfinitePickerView/GWInfinitePickerViewTests/GWInfinitePickerViewTests.m
@@ -0,0 +1,40 @@
+//
+//  GWInfinitePickerViewTests.m
+//  GWInfinitePickerViewTests
+//
+//  Created by Grzegorz Wikiera on 30.03.2015.
+//  Copyright (c) 2015 Grzegorz Wikiera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GWInfinitePickerViewTests : XCTestCase
+
+@end
+
+@implementation GWInfinitePickerViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
